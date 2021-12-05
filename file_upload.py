6c78a419e728890fe17d8c2947ff2a01dbbe52e7
inclue <wb092311@naver.com>
2021-12-05 20:11:43 +0900

    execute 추가

--- file_upload.py
+++ file_upload.py
@@ -8,19 +8,25 @@ app = Flask(__name__)
 
 # 업로드 HTML 렌더링
 @app.route('/upload')
-def render_file():
+def upload():
     return render_template('upload.html')
 
 
 # 파일 업로드 처리
 @app.route('/file_upload', methods=['GET', 'POST'])
-def upload_file():
+def file_upload():
     if request.method == 'POST':
         file = request.files['file']
-        filename = datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.' \
-                   + file.filename.rsplit('.', 1)[1].lower()
+        filename = datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.' + file.filename.rsplit('.', 1)[1].lower()
         file.save(os.path.join(os.getcwd() + '\\uploads', filename))
-    return 'uploads 디렉토리 -> 파일 업로드 성공!'
+    return filename
+
+
+# 함수 실행
+@app.route('/execute', methods=['GET'])
+def execute():
+    parameter_dict = request.args.to_dict()
+    return parameter_dict.get('filename', 'Wrong Parameter')
 
 
 # 서버 실행
